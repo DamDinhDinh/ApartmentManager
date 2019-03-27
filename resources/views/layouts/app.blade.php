2fe0e10b317c494162afbc5fc0cb41efc5b24de9
DamDinhDinh <xifurifeng@gmail.com>
2019-03-27 13:47:27 +0700

    edit apartment index page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('header')
 </head>
 <body>
     <div id="app">
@@ -30,6 +31,9 @@
                 @yield('content')
             </div>
         </main>
+        <footer>
+            @yield('footer')
+        </footer>
     </div>
 </body>
 </html>
--- resources/views/manager/apartment/index.blade.php
+++ resources/views/manager/apartment/index.blade.php
@@ -2,25 +2,28 @@
 
 @section('content')
     <div class="apartment-control-part">
-        <a href="{{route('apartment.create')}}" class="btn btn-primary">Thêm căn hộ mới</a>
-
+        <a href="{{route('apartment.create')}}" class="btn btn-primary float-right">Thêm căn hộ mới</a>
     </div>
     <br>
     <div class="apartment-show-part">
         @if (count($apartmentList) > 0)
-            <table class="col-md-10 table table-striped table-bordered .table-hover thead-dark">
+            <table style="text-align: center !important" class="col-md-10 table table-striped table-bordered .table-hover thead-dark">
                 <tr>
-                    <th>Apartment</th>
-                    <th>Address</th>
+                    <th width="20%">Apartment</th>
+                    <th width="80%">Address</th>
+                    <th width="10%">Edit</th>
+                    <th width="10%">Delete</th>
                 </tr>
                 @foreach ($apartmentList as $apartment)
                     <tr>
                         <td><a href="{{route('apartment.show', ['id' => $apartment->id])}}" >{{$apartment->name}}</a></td>
                         <td>{{$apartment->address}}</td>
+                        <td><a href="{{route('apartment.edit', $apartment->id)}}" class="btn btn-warning">Sửa</a></td>
+                        <td><a onclick="return confirm('Chắc chắn xóa?')" href="{{route('apartment.destroy', $apartment->id)}}" class="btn btn-danger">Xóa</a></td>
                     </tr>
                 @endforeach
-                {{$apartmentList->links()}}
             <table>
+            {{$apartmentList->links()}}
         @else
             <p>None to show.</p>
         @endif
